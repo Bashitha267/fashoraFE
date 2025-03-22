53f56d815f274e7875208322d6867bbc68a37cf9
Nimesh <nimeshspc2k17.gmail.com>
2025-03-22 19:58:12 +0530

    home fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,20 @@
-import { Arrivals } from "../components/Arrivals"
-import { HomeSection } from "../components/HomeSection"
-import { ShopSection } from "../components/ShopSection"
-import '../components/home.css'
+import { Arrivals } from "../components/Arrivals";
+import { HomeSection } from "../components/HomeSection";
+import { ShopSection } from "../components/ShopSection";
+import '../components/home.css';
 
-export const Home = ({navigate_page}) => {
+// Define the type for props
+interface HomeProps {
+  navigate_page: (page: string) => void;
+}
+
+export const Home = ({ navigate_page }: HomeProps) => {
   return (
     <div className="w-screen md:max-w[170vh] mx-auto">
-        <HomeSection/>
-      
-        <Arrivals navigateTo={navigate_page}/>
-        {/* <Bookinghome/> */}
-        <ShopSection/>
+      <HomeSection />
+      <Arrivals navigateTo={navigate_page} />
+      {/* <Bookinghome/> */}
+      <ShopSection />
     </div>
-  )
-}
+  );
+};
