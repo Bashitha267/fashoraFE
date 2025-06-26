3d2d76a246af9645390d4fdb15f37e79adfa3b26
Nimesh <nimeshspc2k17.gmail.com>
2025-06-26 21:29:52 +0530

    edited h

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -4,11 +4,11 @@ import logo from '../assets/logo2.jpg';
 export const Signin = () => {
   return (
     <div className="md:mt-55 mt-38 w-full md:max-w-[160vh] mx-auto px-4 h-screen">
-      <div className="lg:grid lg:grid-cols-3 lg:w-11/12 lg:mx-auto lg:h-full">
-        <div className="lg:col-span-2 bg-amber-300 lg:flex lg:justify-center lg:items-center">
-          <div className="bg-white lg:w-2/3 lg:h-3/5 lg:flex lg:flex-col lg:p-4 p-2">
+      <div className="lg:grid lg:grid-cols-3 lg:w-11/12 lg:mx-auto lg:h-full relative">
+        <div className="lg:col-span-2  lg:flex lg:justify-center lg:items-center">
+          <div className="bg-white lg:w-2/3 h-full lg:h-3/5 lg:flex lg:flex-col lg:p-5 p-2 shadow-2xl">
           <div className="flex justify-center lg:justify-start"><img src={logo} className="w-52 h-32 mb-5 object-contain"></img></div>
-          <div className="flex justify-center lg:justify-start text-xl mb-4 font-bold text-gray-400">Welcome Back!!</div>
+          <div className="flex justify-center lg:justify-start text-2xl mb-4 font-bold text-gray-400">Welcome Back!!</div>
           <div className="flex flex-col">
             <div className="flex-col flex">
               <label>Username</label>
@@ -26,7 +26,7 @@ export const Signin = () => {
           </div>
         </div>
         <div className="col-span-1 bg-[#444444]"></div>
-
+        <div className="absolute hidden  lg:flex bottom-0 left-1/2 right-0.5 bg-red-200 " >img</div>
 
       </div>
 
